f875cfda8e1a66b0e8000999585fc039486d0bf3
Auriga Aristo <aurigaaristo@gmail.com>
2022-04-27 00:56:52 +0700

    :bug: fix: scrap data success

--- src/KBBI.php
+++ src/KBBI.php
@@ -5,25 +5,33 @@ namespace Dipantry\Kbbi;
 use Dipantry\Kbbi\Exception\KbbiResponseException;
 use Dipantry\Kbbi\Util\SearchWord;
 use Exception;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\JsonResponse;
 use Rigsto\ApiHttpStatus\ApiResources;
 use Rigsto\ApiHttpStatus\HttpStatus;
 
 class KBBI extends SearchWord
 {
-    public function search(string $word): JsonResponse
+    public function search(string $word, string $session = ""): JsonResponse
     {
         try {
-            $response = $this->searchWord($word);
-        } catch (KbbiResponseException $e){
+            $response = $this->searchWord($word, $session);
+        } catch (KbbiResponseException $e) {
             return ApiResources::generateResponse(
                 HttpStatus::BAD_REQUEST,
                 $e->getMessage()
             );
-        } catch (Exception){
+        } catch (GuzzleException $e){
+            return ApiResources::generateResponse(
+                HttpStatus::BAD_REQUEST,
+                'Request Error',
+                $e->getMessage()
+            );
+        } catch (Exception $e){
             return ApiResources::generateResponse(
                 HttpStatus::INTERNAL_SERVER_ERROR,
-                'Internal Server Error'
+                'Internal Server Error',
+                $e->getMessage()
             );
         }
 
--- src/Util/SearchWord.php
+++ src/Util/SearchWord.php
@@ -15,11 +15,15 @@ class SearchWord
     private string $kbbiUrl = 'https://kbbi.kemdikbud.go.id/entri';
 
     /* @throws GuzzleException|KbbiResponseException */
-    private function getHttp($url): StreamInterface
+    private function getHttp($url, string $session): StreamInterface
     {
         try {
             $httpClient = new Client();
-            $response = $httpClient->get($this->kbbiUrl . $url);
+            $response = $httpClient->get($this->kbbiUrl . $url, [
+                'headers' => [
+                    'Cookie' => '.AspNet.ApplicationCookie=' . $session,
+                ]
+            ]);
         } catch (Exception){
             throw new KbbiResponseException('Koneksi ke KBBI gagal');
         }
@@ -28,25 +32,29 @@ class SearchWord
     }
 
     /* @throws KbbiResponseException|GuzzleException */
-    protected function searchWord(string $word): array
+    protected function searchWord(string $word, string $session): array
     {
         libxml_use_internal_errors(true);
-        $htmlString = $this->getHttp("/{$word}");
+        $htmlString = $this->getHttp("/{$word}", $session);
 
         $doc = new DOMDocument();
         $doc->loadHTML($htmlString);
         $xpath = new DOMXPath($doc);
 
         $results = [];
-        foreach ($doc->getElementsByTagName('h2') as $h2) {
+        foreach ($doc->getElementsByTagName('h2') as $i => $h2) {
             $result['spelling'] = $h2->nodeValue;
 
             $meaning_array = [];
-            foreach ($xpath->query('//ol//li') as $meaning){
+            $list = $xpath->query("//ol[contains(@class, 'last-list-child')]//li");
+            foreach ($list as $j => $meaning){
+                if ($j == $list->count() - 1){
+                    continue;
+                }
                 $mean['description'] = $meaning->childNodes->item(1)->nodeValue;
 
                 $category_array = [];
-                foreach ($xpath->query('.//span', $meaning) as $spans){
+                foreach ($xpath->query('.//font//i//span', $meaning) as $k => $spans){
                     $category['code'] = $spans->nodeValue;
                     $category['description'] = $spans->attributes->getNamedItem('title')->nodeValue;
 
