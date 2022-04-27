dd7b5b23ec5eb4999f2c54f4968175283324a751
Auriga Aristo <aurigaaristo@gmail.com>
2022-04-27 14:07:21 +0700

    :zap: clean code

--- src/Util/SearchWord.php
+++ src/Util/SearchWord.php
@@ -4,6 +4,7 @@ namespace Dipantry\Kbbi\Util;
 
 use Dipantry\Kbbi\Exception\KbbiResponseException;
 use DOMDocument;
+use DOMNodeList;
 use DOMXPath;
 use Exception;
 use GuzzleHttp\Client;
@@ -13,6 +14,8 @@ use Psr\Http\Message\StreamInterface;
 class SearchWord
 {
     private string $kbbiUrl = 'https://kbbi.kemdikbud.go.id/entri';
+    private $doc;
+    private $xpath;
 
     /* @throws GuzzleException|KbbiResponseException */
     private function getHttp($url, string $session): StreamInterface
@@ -37,38 +40,53 @@ class SearchWord
         libxml_use_internal_errors(true);
         $htmlString = $this->getHttp("/{$word}", $session);
 
-        $doc = new DOMDocument();
-        $doc->loadHTML($htmlString);
-        $xpath = new DOMXPath($doc);
+        $this->doc = new DOMDocument();
+        $this->doc->loadHTML($htmlString);
+        $this->xpath = new DOMXPath($this->doc);
 
+        return $this->processResult($this->doc->getElementsByTagName('h2'));
+    }
+
+    private function processResult(DOMNodeList $h2s): array {
         $results = [];
-        foreach ($doc->getElementsByTagName('h2') as $i => $h2) {
+        foreach ($h2s as $index => $h2){
             $result['spelling'] = $h2->nodeValue;
 
             $meaning_array = [];
-            $list = $xpath->query("//ol[contains(@class, 'last-list-child')]//li");
-            foreach ($list as $j => $meaning){
-                if ($j == $list->count() - 1){
-                    continue;
-                }
-                $mean['description'] = $meaning->childNodes->item(1)->nodeValue;
+            $list = $this->xpath->query("//ol[contains(@class, 'last-list-child')]//li");
 
-                $category_array = [];
-                foreach ($xpath->query('.//font//i//span', $meaning) as $k => $spans){
-                    $category['code'] = $spans->nodeValue;
-                    $category['description'] = $spans->attributes->getNamedItem('title')->nodeValue;
+            $result['meanings'] = $this->processMeaning($list);
 
-                    $category_array[] = $category;
-                }
-                $mean['categories'] = $category_array;
-
-                $meaning_array[] = $mean;
-            }
-
-            $result['meanings'] = $meaning_array;
             $results[] = $result;
         }
-
         return $results;
+    }
+
+    private function processMeaning(DOMNodeList $meanings): array {
+        $meaning_array = [];
+        foreach ($meanings as $index => $meaning){
+            if ($index == $meanings->count() - 1){
+                continue;
+            }
+
+            $mean['description'] = $meaning->childNodes->item(1)->nodeValue;
+            $mean['categories'] = $this->processCategory(
+                $this->xpath->query('.//font//i//span', $meaning)
+            );
+
+            $meaning_array[] = $mean;
+        }
+        return $meaning_array;
+    }
+
+    private function processCategory(DOMNodeList $categories): array {
+        $category_array = [];
+        foreach ($categories as $index => $category){
+            $cat['code'] = $category->nodeValue;
+            $cat['description'] = $category->attributes->getNamedItem('title')->nodeValue;
+
+            $category_array[] = $cat;
+        }
+        return $category_array;
     }
 }
